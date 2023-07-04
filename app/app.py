e0bd9b25cd0f08776944666c321fce9eca1d2982
vincenttommi <vincenttommi@gmail.com>
2023-07-04 13:01:13 +0300

    updating seed.py

--- app/app.py
+++ app/app.py
@@ -1,1 +1,26 @@
 from flask import Flask
+from  flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from models import db
+
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE'] = 'sqlite:///cars.db'
+
+
+
+migrate  =  Migrate(app,db)
+#class  provided by  flask-migrate that  handles database migrations in flask
+db.init_app(app)
+# establishes the connectiona and configure the database object to work
+# with flask application
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    app.run(port=5555)
